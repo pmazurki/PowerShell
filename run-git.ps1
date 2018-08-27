3b5b207994cc20a30b5ced0ffaf953691098b178
pmazurki <pmazurki@me.com>
2018-08-27 12:39:03 +0200

    Update run-git.ps1

--- run-git.ps1
+++ run-git.ps1
@@ -1,5 +1,9 @@
-$specific="pmazurki/PowerShell/master/Downloader.ps1"
+$specific ="pmazurki/PowerShell/master/Downloader.ps1"
+
+$url= "https://raw.githubusercontent.com/"+ $specific
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$sourceurl = Invoke-WebRequest https://raw.githubusercontent.com/+ $specific
+
+$sourceurl = Invoke-WebRequest $url
+
 Invoke-Expression $($sourceurl.Content)
